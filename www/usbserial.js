221d9f0f27e22ce4fbff044831b6bc5741d2eb34
dariocavada <dario.cavada.lab@gmail.com>
2018-02-26 16:57:44 +0100

    Create UsbSerial repository

---
+++ www/usbserial.js
@@ -0,0 +1,75 @@
+var exec = require('cordova/exec');
+
+var usbserial = {
+    requestPermission: function(opts, successCallback, errorCallback) {
+        if (typeof opts === 'function') {  //user did not pass opts
+          errorCallback = successCallback;
+          successCallback = opts;
+          opts = {};
+        }
+        cordova.exec(
+            successCallback,
+            errorCallback,
+            'UsbSerial',
+            'requestPermission',
+            [{'opts': opts}]
+        );
+    },
+    open: function(opts, successCallback, errorCallback) {
+        cordova.exec(
+            successCallback,
+            errorCallback,
+            'UsbSerial',
+            'openSerial',
+            [{'opts': opts}]
+        );
+    },
+    write: function(data, successCallback, errorCallback) {
+        cordova.exec(
+            successCallback,
+            errorCallback,
+            'UsbSerial',
+            'writeSerial',
+            [{'data': data}]
+        );
+    },
+    writeHex: function(hexString, successCallback, errorCallback) {
+        cordova.exec(
+            successCallback,
+            errorCallback,
+            'UsbSerial',
+            'writeSerialHex',
+            [{'data': hexString}]
+        );
+    },
+    read: function(successCallback, errorCallback) {
+        cordova.exec(
+            successCallback,
+            errorCallback,
+            'UsbSerial',
+            'readSerial',
+            []
+        );
+    },
+    close: function(successCallback, errorCallback) {
+        cordova.exec(
+            successCallback,
+            errorCallback,
+            'Serial',
+            'closeSerial',
+            []
+        );
+    },
+    registerReadCallback: function(successCallback, errorCallback) {
+        cordova.exec(
+            successCallback,
+            errorCallback,
+            'UsbSerial',
+            'registerReadCallback',
+            []
+        );
+    }
+
+};
+module.exports = usbserial;
+
